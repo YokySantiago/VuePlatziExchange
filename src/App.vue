93be4875b2af32de1c40e0ec18343407ab345b40
Yoky Code <yoky@code.com>
2020-12-04 21:16:43 -0500

    Se agrega tailwind, se agrregan componentes del contenido principal

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <main>
+    <px-header />
+    <px-assetsTable />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from "@/components/PxHeader";
+import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    PxHeader,
+    PxAssetsTable,
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
